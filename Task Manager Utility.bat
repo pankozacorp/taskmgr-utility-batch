a3a2371452d7ec5cb5e34a825a7e77123375ab69
pankoza-pl <77279607+pankoza-pl@users.noreply.github.com>
2021-12-17 21:02:28 +0100

    Initial commit

---
+++ Task Manager Utility.bat
@@ -0,0 +1,25 @@
+@echo off
+title Task Manager Utility by Creper132 (modded by pankoza-pl) (Run as Administrator)
+:main
+cls
+echo (1) Turn on task manager
+echo (2) Turn off task manager
+echo (3) Kill task manager
+echo (4) Start Task Manager
+set /p select=Select: 
+if %select%==1 goto 1
+if %select%==2 goto 2
+if %select%==3 goto 3
+if %select%==4 goto 4
+:1
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 0 /f
+goto main
+:2
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
+goto main
+:3
+taskkill /f /im taskmgr.exe /t
+goto main
+:4
+start taskmgr.exe
+goto main
